b6a8d413867adba8d718e79dbe3e0c8a81d6c042
cmp-cc <baiwoyidao@163.com>
2016-08-17 15:52:15 +0800

    vue devflow config

---
+++ src/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="main">
+        Hello, Vue DevFlow
+    </div>
+
+    <!-- main view -->
+    <router-view
+            class="view"
+            keep-alive
+            transition
+            transition-mode="out-in">
+    </router-view>
+</template>
+
+
+<style>
+    #main {
+        margin: 15px 0 auto;
+        text-align: center;
+        font-size: 19px;
+    }
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import App from './components/App.vue'
+
+Vue.use(Router)
+
+var router = new Router()
+
+router.map({
+})
+
+router.start(App, '#app')
